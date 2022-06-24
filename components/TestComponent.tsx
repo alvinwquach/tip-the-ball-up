29d3dd607164352b5629dece36dc060e44bac9ba
Alvin Quach <alvinwquach@gmail.com>
2022-06-24 12:00:40 -0700

    created test component to transform array of players into an array of next paths

--- components/TestComponent.tsx
+++ components/TestComponent.tsx
@@ -1,20 +1,43 @@
 import React, { useState, useEffect } from 'react'
-import { getAllPlayers } from '../basketballapi'
+import { getAllPlayers, PagePlayer } from '../basketballapi'
 
 function TestComponent() {
-  const [playerPage, setPlayerPage] = useState<any[]>([])
+  const [playerPage, setPlayerPage] = useState<PagePlayer[]>([])
 
   useEffect(() => {
     const getFirstPage = async () => {
+      // get 200 players from api
       const allPlayers = await getAllPlayers()
       const paths = allPlayers
+      // set player page of 200 players from api so that
+      // we can read it outside of this effect from the playerPage
+      // variable
       setPlayerPage(paths)
     }
     getFirstPage()
   }, [])
 
-  return <pre>{JSON.stringify(playerPage)}</pre>
+  // playerPage already has 200 players
+  // i need to transform (map) those player objects
+  // into the shape that NextJS wants
+
+  const playerPaths: NextPaths = playerPage.map((player) => {
+    //
+    // tiptheball.com/players/stephen-curry
+    const fullName = player.first_name + `-` + player.last_name
+    return {
+      params: {
+        playername: fullName.toLowerCase(),
+      },
+    }
+  })
+
+  return <pre>{JSON.stringify(playerPaths)}</pre>
 }
+
+type NextPaths = {
+  params: any
+}[]
 
 // export async function getStaticPaths() {
 //   const players = players.map((player) => {
@@ -49,4 +72,3 @@ export default TestComponent
 // const paths = players.map((player) => {
 
 // })
-
