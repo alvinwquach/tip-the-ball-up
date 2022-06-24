23f859c7b6909f0f65b22e6234638670dd772bec
Alvin Quach <alvinwquach@gmail.com>
2022-06-24 15:03:18 -0700

    created player cache

--- pages/players/[playername].tsx
+++ pages/players/[playername].tsx
@@ -1,12 +1,18 @@
 import Head from 'next/head'
-import { getAllPlayers } from '../../basketballapi'
+import { getAllPlayers, PagePlayer } from '../../basketballapi'
+import playerCache from '../../playercache'
 
-export default function Player() {
+type PlayerProps = {
+  player: PagePlayer
+}
+
+export default function Player({ player }: PlayerProps) {
   return (
     <>
       <Head>
-        <title>Steph Curry | Tip The Ball Up</title>
+        <title> Tip The Ball Up</title>
       </Head>
+      <pre>{JSON.stringify(player)}</pre>
     </>
   )
 }
@@ -14,6 +20,9 @@ export default function Player() {
 export async function getStaticPaths() {
   // paths array of player names transformed from a list of all players
   const players = await getAllPlayers()
+  // write the cache to a file
+  playerCache.cache.set(players)
+
   const paths = players.map((player) => {
     const fullName = player.first_name + `-` + player.last_name
     return {
@@ -25,13 +34,21 @@ export async function getStaticPaths() {
 
   return {
     paths: paths,
-    fallback: false, // false or 'blocking'
+    fallback: true, // false or 'blocking'
   }
 }
 
 export async function getStaticProps(context: any) {
   // takes the params object, returns an object with props
+  // .find returns a boolean (returns first element of the array that returns true)
+  const cache = await playerCache.cache.get()
+  const player = cache.find((cachedPlayer) => {
+    const fullName = cachedPlayer.first_name + `-` + cachedPlayer.last_name
+    return context.params.playername === fullName.toLowerCase()
+  })
   return {
-    props: {},
+    props: {
+      player,
+    },
   }
 }
---
+++ playercache.ts
@@ -0,0 +1,25 @@
+import { promises as fs } from 'fs'
+import path from 'path'
+import { PagePlayer } from './basketballapi'
+
+const filename = 'players.db'
+
+export const playerCache = {
+  cache: {
+    get: async (): Promise<PagePlayer[]> => {
+      // generating location of file, cwd = current working directory
+      const data = await fs.readFile(path.join(process.cwd(), filename))
+      // after it reads, parsed json
+      const players: PagePlayer[] = JSON.parse(data as unknown as string)
+      return players
+    },
+    set: async (players: PagePlayer[]) => {
+      return await fs.writeFile(
+        path.join(process.cwd(), filename),
+        JSON.stringify(players)
+      )
+    },
+  },
+}
+
+export default playerCache
